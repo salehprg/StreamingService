dde970ca2b125c9b11472e226fc93fd5f694f54c
Hamid Najafi <goldenstarc@gmail.com>
2020-12-29 20:09:24 +0000

    Add new file

---
+++ build.sh
@@ -0,0 +1,8 @@
+sudo git stash
+sudo git pull origin master
+dotnet publish --configuration Release
+sudo systemctl stop kestrel-livestream.service
+sudo rm -rf /var/www/streamingservice
+mkdir /var/www/streamingservice
+sudo cp -R /root/streamingservice/bin/Release/netcoreapp3.1/publish/* /var/www/streamingservice/
+sudo systemctl start kestrel-livestream.service
