68ddeff72a2ed459f51c66fb3112b3d3dda74ce8
saleh_prg <salehebrahimian@gmail.com>
2020-12-30 22:36:40 +0330

    Create Rooms at StartUp

--- Startup.cs
+++ Startup.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -88,6 +89,11 @@ namespace streamingservice
                     ValidateLifetime = false
                 };
             });
+
+            if(!Directory.Exists("Rooms"))
+            {   
+                Directory.CreateDirectory("Rooms");
+            }
 
             services.AddControllers();
         }
